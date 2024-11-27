4cc53cf6e49d12fa62193fbfa44346ed9fe2d72f
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-28 04:02:47 +0800

    init instance

---
+++ test.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+import delay from 'delay';
+import pAll from './index.js';
+
+// See `p-map` for more comprehensive tests.
+test('main', async t => {
+	const input = [
+		async () => 1,
+		async () => {
+			await delay(100);
+			return 2;
+		},
+		() => 3,
+		async () => 4,
+	];
+
+	t.deepEqual(await pAll(input), [1, 2, 3, 4]);
+});
+
+test('handles empty iterable', async t => {
+	t.deepEqual(await pAll([]), []);
+});
